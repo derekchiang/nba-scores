b6a5fb340a2402d0b3c4a8c1db88199e5ffb6884
Derek Chiang <derekchiang93@gmail.com>
2013-12-24 01:12:01 +0800

    Adding error handling

--- main.rs
+++ main.rs
@@ -6,6 +6,7 @@ extern mod http;
 
 use std::io::buffered::BufferedReader;
 use std::os::set_exit_status;
+use std::str::not_utf8;
 
 use extra::json;
 use extra::json::{List, Object, String, Number};
@@ -13,6 +14,7 @@ use extra::json::{List, Object, String, Number};
 use http::client::RequestWriter;
 use http::method::Get;
 
+static ESPN_URL: &'static str = "http://espn.go.com/nba/";
 static PATTERN: &'static str = "var sbMaster = ";
 
 fn fail() {
@@ -32,51 +34,50 @@ macro_rules! take_or_fail(($val:expr, $ok:pat => $out:expr) => {
 })
 
 fn main() {
-    let request = RequestWriter::new(Get, from_str("http://espn.go.com/nba/").unwrap());
+    let request = RequestWriter::new(Get, from_str(ESPN_URL).unwrap());
     let mut response = match request.read_response() {
         Ok(response) => BufferedReader::new(response),
         Err(_) => unreachable!(),
     };
-
-    for line in response.lines() {
-        if line.find_str(PATTERN).is_none() {
-            continue;
-        }
-        let val = take_or_fail!(json::from_str(line.slice_from(PATTERN.len())), Ok(val) => val);
-        let mut obj = take_or_fail!(val, Object(obj) => obj);
-        let lst = take_or_fail!(obj.pop(&~"sports"), Some(List(lst)) => lst);
-        for sport in lst.move_rev_iter() {
-            let mut obj = take_or_fail!(sport, Object(obj) => obj);
-            let s = take_or_fail!(obj.pop(&~"sport"), Some(String(s)) => s);
-            if "nba" != s { continue; }
-            let lst = take_or_fail!(obj.pop(&~"leagues"), Some(List(lst)) => lst);
-            let mut obj = take_or_fail!(lst[0], Object(obj) => obj);
-            let games = take_or_fail!(obj.pop(&~"games"), Some(List(games)) => games);
-            for game in games.move_rev_iter() {
-                let mut obj = take_or_fail!(game, Object(obj) => obj);
-                let status = take_or_fail!(obj.pop(&~"status"), Some(Number(status)) => status);
-                match status {
-                    1.0 => {
-                        let status_text = take_or_fail!(obj.pop(&~"statusText"), Some(String(status_text)) => status_text);
-                        print!("Incoming ({}): ", status_text);
-                    },
-                    3.0 => {
-                        print("Final: ");
-                    },
-                    _ => {
-                        print("Live: ");
-                    }
-                };
-                let mut home = take_or_fail!(obj.pop(&~"home"), Some(Object(home)) => home);
-                let (location, nickname, score) = take_or_fail!((home.pop(&~"location"), home.pop(&~"nickname"), home.pop(&~"score")),
-                    (Some(String(location)), Some(String(nickname)), Some(Number(score))) => (location, nickname, score));
-                print!("{} {}(home) {} : ", location, nickname, score);
-                let mut away = take_or_fail!(obj.pop(&~"away"), Some(Object(away)) => away);
-                let (location, nickname, score) = take_or_fail!((away.pop(&~"location"), away.pop(&~"nickname"), away.pop(&~"score")),
-                    (Some(String(location)), Some(String(nickname)), Some(Number(score))) => (location, nickname, score));
-                println!("{} {} {}(away)", score, location, nickname);
+    not_utf8::cond.trap(|_| {~""}).inside(|| {
+        for line in response.lines() {
+            if line.find_str(PATTERN).is_none() { continue; }
+            let val = take_or_fail!(json::from_str(line.slice_from(PATTERN.len())), Ok(val) => val);
+            let mut obj = take_or_fail!(val, Object(obj) => obj);
+            let lst = take_or_fail!(obj.pop(&~"sports"), Some(List(lst)) => lst);
+            for sport in lst.move_rev_iter() {
+                let mut obj = take_or_fail!(sport, Object(obj) => obj);
+                let s = take_or_fail!(obj.pop(&~"sport"), Some(String(s)) => s);
+                if "nba" != s { continue; }
+                let lst = take_or_fail!(obj.pop(&~"leagues"), Some(List(lst)) => lst);
+                let mut obj = take_or_fail!(lst[0], Object(obj) => obj);
+                let games = take_or_fail!(obj.pop(&~"games"), Some(List(games)) => games);
+                for game in games.move_rev_iter() {
+                    let mut obj = take_or_fail!(game, Object(obj) => obj);
+                    let status = take_or_fail!(obj.pop(&~"status"), Some(Number(status)) => status);
+                    match status {
+                        1.0 => {
+                            let status_text = take_or_fail!(obj.pop(&~"statusText"), Some(String(status_text)) => status_text);
+                            print!("Incoming ({}): ", status_text);
+                        },
+                        3.0 => {
+                            print("Final: ");
+                        },
+                        _ => {
+                            print("Live: ");
+                        }
+                    };
+                    let mut home = take_or_fail!(obj.pop(&~"home"), Some(Object(home)) => home);
+                    let (location, nickname, score) = take_or_fail!((home.pop(&~"location"), home.pop(&~"nickname"), home.pop(&~"score")),
+                        (Some(String(location)), Some(String(nickname)), Some(Number(score))) => (location, nickname, score));
+                    print!("{} {}(home) {} : ", location, nickname, score);
+                    let mut away = take_or_fail!(obj.pop(&~"away"), Some(Object(away)) => away);
+                    let (location, nickname, score) = take_or_fail!((away.pop(&~"location"), away.pop(&~"nickname"), away.pop(&~"score")),
+                        (Some(String(location)), Some(String(nickname)), Some(Number(score))) => (location, nickname, score));
+                    println!("{} {} {}(away)", score, location, nickname);
+                }
+                return;
             }
-            return;
         }
-    }
+    });
 }
